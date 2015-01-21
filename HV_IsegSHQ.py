8cf754500fd79c04119ea9539f8ec6205c093f89
Luis Ardila <leardilap@unal.edu.co>
2015-01-21 16:12:46 +0100

    updated for single source and tested with hardware

--- HV_IsegSHQ.py
+++ HV_IsegSHQ.py
@@ -1,7 +1,11 @@
-# Luis Ardila 	leardilap@unal.edu.co 	21/11/14
+#! /usr/bin/env python
+
+# Luis Ardila 	leardilap@unal.edu.co 	21/01/15
 
 import sys
 from PyQt4 import QtGui, QtCore, uic
+
+from time import sleep
 
 tangoEnable = True
 if tangoEnable:
@@ -16,47 +20,67 @@ class HV_Control(QtGui.QWidget):
 		
 		#Declaring Device
 		if tangoEnable:
-			self.axis = PyTango.DeviceProxy("iss/detlab/hv2")
+			self.HV = PyTango.DeviceProxy("iss/detlab/hv2")
 				
 		#CH 1
 		self.connect(self.ui.GetVoltage1, QtCore.SIGNAL("clicked()"), self.GetVoltage1)
+		self.connect(self.ui.DesVoltage1, QtCore.SIGNAL("valueChanged(double)"), self.SetTargetVoltage1)
 		self.connect(self.ui.SetVoltage1, QtCore.SIGNAL("clicked()"), self.SetVoltage1)
 		self.connect(self.ui.GetRamp1, QtCore.SIGNAL("clicked()"), self.GetRamp1)
 		self.connect(self.ui.SetRamp1, QtCore.SIGNAL("clicked()"), self.SetRamp1)
 		self.connect(self.ui.Disable1, QtCore.SIGNAL("clicked()"), self.Disable1)
 		
-		#CH 2
-		self.connect(self.ui.GetVoltage2, QtCore.SIGNAL("clicked()"), self.GetVoltage2)
-		self.connect(self.ui.SetVoltage2, QtCore.SIGNAL("clicked()"), self.SetVoltage2)
-		self.connect(self.ui.GetRamp2, QtCore.SIGNAL("clicked()"), self.GetRamp2)
-		self.connect(self.ui.SetRamp2, QtCore.SIGNAL("clicked()"), self.SetRamp2)
-		self.connect(self.ui.Disable2, QtCore.SIGNAL("clicked()"), self.Disable2)
+		self.ID = self.HV.getDeviceID()
 		
-		#Updating Voltage and Current
+		if str(self.ID) == "484215;3.09;4000V;3mA":		# This HV has only one port
+			self.dual = False
+		else:
+			self.dual = True
+		
+		if self.dual:	
+			#CH 2
+			self.connect(self.ui.GetVoltage2, QtCore.SIGNAL("clicked()"), self.GetVoltage2)
+			self.connect(self.ui.DesVoltage2, QtCore.SIGNAL("valueChanged(double)"), self.SetTargetVoltage2)
+			self.connect(self.ui.SetVoltage2, QtCore.SIGNAL("clicked()"), self.SetVoltage2)
+			self.connect(self.ui.GetRamp2, QtCore.SIGNAL("clicked()"), self.GetRamp2)
+			self.connect(self.ui.SetRamp2, QtCore.SIGNAL("clicked()"), self.SetRamp2)
+			self.connect(self.ui.Disable2, QtCore.SIGNAL("clicked()"), self.Disable2)
+		else:
+			self.ui.GetVoltage2.setEnabled(False)
+			self.ui.SetVoltage2.setEnabled(False)
+			self.ui.GetRamp2.setEnabled(False)
+			self.ui.SetRamp2.setEnabled(False)
+			self.ui.Disable2.setEnabled(False)
+			self.ui.DesVoltage2.setEnabled(False)
+			self.ui.DesRamp2.setEnabled(False)
+		
+					
+		# Updating Voltage and Current
 		self.timer = QtCore.QTimer()
-		self.timer.start(300)
+		self.timer.start(2000)
 		self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.Update)
 		
 	# CH 1	   
 	
 	def GetVoltage1(self):
 		if tangoEnable:
-			voltage = self.HV_Control.getTargetVoltage1()
+			voltage = self.HV.getTargetVoltage1()
 		else:
 			voltage = 1
 		self.ui.DesVoltage1.setValue(voltage)
-		
-	def SetVoltage1(self):
+	
+	def SetTargetVoltage1(self):
 		voltage = self.ui.DesVoltage1.value()
 		if tangoEnable:
-			self.HV_Control.setTargetVoltage1(voltage)
-			self.HV_Control.changeVoltage1()
-		else:
-			self.ui.Voltage1.display(voltage)
+			self.HV.setTargetVoltage1(voltage)
+			
+	def SetVoltage1(self):
+		if tangoEnable:
+			self.HV.changeVoltage1()
 		
 	def GetRamp1(self):
 		if tangoEnable:
-			ramp = self.HV_Control.getVoltageRamp1()
+			ramp = self.HV.getVoltageRamp1()
 		else:
 			ramp = 1
 		self.ui.DesRamp1.setValue(ramp)
@@ -64,17 +88,18 @@ class HV_Control(QtGui.QWidget):
 	def SetRamp1(self):
 		ramp = self.ui.DesRamp1.value()
 		if tangoEnable:
-			self.HV_Control.setVoltageRamp1(ramp)
+			self.HV.setVoltageRamp1(ramp)
 		
 	def Disable1(self):
 		if tangoEnable:
 			if self.ui.Disable1.isChecked():
-				self.HV_Control.setTargetVoltage1(0)
-				self.HV_Control.changeVoltage1()
+				self.HV.setTargetVoltage1(0)
+				sleep(1)
+				self.SetVoltage1()
 			else:
-				voltage = self.ui.DesVoltage1.value()
-				self.HV_Control.setTargetVoltage1(voltage)
-				self.HV_Control.changeVoltage1()
+				self.SetTargetVoltage1()
+				sleep(1)
+				self.SetVoltage1()
 		else:
 			if self.ui.Disable1.isChecked():
 				self.ui.Voltage1.display(0)
@@ -88,22 +113,23 @@ class HV_Control(QtGui.QWidget):
 	
 	def GetVoltage2(self):
 		if tangoEnable:
-			voltage = self.HV_Control.getTargetVoltage2()
+			voltage = self.HV.getTargetVoltage2()
 		else:
 			voltage = 1
 		self.ui.DesVoltage2.setValue(voltage)
-		
-	def SetVoltage2(self):
+	
+	def SetTargetVoltage2(self):
 		voltage = self.ui.DesVoltage2.value()
 		if tangoEnable:
-			self.HV_Control.setTargetVoltage2(voltage)
-			self.HV_Control.changeVoltage2()
-		else:
-			self.ui.Voltage2.display(voltage)
+			self.HV.setTargetVoltage2(voltage)
+			
+	def SetVoltage2(self):
+		if tangoEnable:
+			self.HV.changeVoltage2()
 		
 	def GetRamp2(self):
 		if tangoEnable:
-			ramp = self.HV_Control.getVoltageRamp2()
+			ramp = self.HV.getVoltageRamp2()
 		else:
 			ramp = 1
 		self.ui.DesRamp2.setValue(ramp)
@@ -111,17 +137,18 @@ class HV_Control(QtGui.QWidget):
 	def SetRamp2(self):
 		ramp = self.ui.DesRamp2.value()
 		if tangoEnable:
-			self.HV_Control.setVoltageRamp2(ramp)
+			self.HV.setVoltageRamp2(ramp)
 		
 	def Disable2(self):			
 		if tangoEnable:
 			if self.ui.Disable2.isChecked():
-				self.HV_Control.setTargetVoltage2(0)
-				self.HV_Control.changeVoltage2()
+				self.HV.setTargetVoltage2(0)
+				sleep(1)
+				self.SetVoltage2()
 			else:
-				voltage = self.ui.DesVoltage2.value()
-				self.HV_Control.setTargetVoltage2(voltage)
-				self.HV_Control.changeVoltage2()
+				self.SetTargetVoltage2()
+				sleep(1)
+				self.SetVoltage2()
 		else:
 			if self.ui.Disable2.isChecked():
 				self.ui.Voltage2.display(0)
@@ -135,23 +162,23 @@ class HV_Control(QtGui.QWidget):
 	
 	def Update(self):
 		if tangoEnable:
-			if self.HV_Control.getStatus1():
-				self.ui.State2.setText("ON")
-				current = self.HV_Control.getCurrent1()
-				self.ui.Current1.display(current)
-				voltage = self.HV_Control.getVoltage1()
+			if self.HV.getStatus1() == "ON\r":
+				self.ui.State1.setText("ON")
+				current = self.HV.getCurrent1()
+				self.ui.Current1.display(float(current))
+				voltage = self.HV.getVoltage1()
 				self.ui.Voltage1.display(voltage)
 			else:
 				self.ui.State1.setText("OFF")
 				
-			if self.HV_Control.getStatus2():
+			if self.HV.getStatus2() == "ON\r" and self.dual:
 				self.ui.State2.setText("ON")
-				current = self.HV_Control.getCurrent2()
-				self.ui.Current2.display(current)
-				voltage = self.HV_Control.getVoltage2()
+				current = self.HV.getCurrent2()
+				self.ui.Current2.display(float(current))
+				voltage = self.HV.getVoltage2()
 				self.ui.Voltage2.display(voltage)
 			else:
-				self.ui.State1.setText("OFF")
+				self.ui.State2.setText("OFF")
 				
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
